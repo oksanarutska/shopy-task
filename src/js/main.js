97669adffda15b688eac3e4ea219fab2f0f022ed
Anastasiia Yemets <40547968+anastasiia-emets@users.noreply.github.com>
2019-02-12 18:27:55 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
-
-
-
 document.addEventListener('click', (e) => {
 	let NumberCount = document.querySelector('.product-card-item2__information_number');
 	let counter = document.querySelector('.product-card-item2__information_number').innerHTML;
-
 	if (e.target.matches('#plus')) {
 		counter++;
 		if (NumberCount) {
@@ -19,4 +15,60 @@ document.addEventListener('click', (e) => {
 		}
 	}
 })
+
+class Slider {
+	constructor({ root, images}) {
+		this.root = root;
+		this.images = images;
+		this.render();
+		document.querySelector('.thumb').addEventListener('click', (e) => {
+			console.log(e.target);
+		})
+	}
+	render() {
+		this.root.innerHTML =
+			`
+			<img class="big" src="${this.images[2].big}">
+		${
+			this.images.map((image, index) => {
+				return `<img class="thumb" data-index-number="${index}" src="${image.thumb}">`
+			})
+			}`
+	
+
+	}
+
+	setListeners() {
+		
+			if (e.target.matches('.thumb')) {
+
+				var idSrc = e.target.dataset.indexNumber;
+				console.log(e.target);
+		
+				render();
+
+			};
+	
+	}
+}
+const slider = new Slider({
+	root:
+		document.querySelector('.product-card-item1'),
+	images: [
+		{
+			thumb: '../images/slider-3.svg',
+			big: '../images/slider2.jpg'
+		},
+		{
+			thumb: '../images/slider3.jpg',
+			big: '../images/slider-2.svg'
+		},
+		{
+			thumb: '../images/slider-1.jpg',
+			big: '../images/slide-1.svg'
+		},
+	]
+
+});
+
 
